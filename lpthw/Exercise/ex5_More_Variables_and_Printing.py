14bc6fe72ec85402a8e62c779a07f72ed4ceb1b6
Abhishek Baliyan <abhishek.baliyan.code@gmail.com>
2020-08-22 12:54:26 +0530

    Example 5 - Variables and Printing

---
+++ lpthw/Exercise/ex5_More_Variables_and_Printing.py
@@ -0,0 +1,18 @@
+my_name = 'Abhishek Baliyan'
+my_age = 26
+my_height = 5.7
+my_weight = 57
+my_eyes = 'Black'
+my_teeth = 'White'
+my_hair = 'Black'
+
+print(f"Let's talk about {my_name}.")
+print(f"He's {my_height} foot tall.")
+print(f"He's {my_weight} kg heavy.")
+print("Actually that's not too heavy.")
+print(f"He's got {my_eyes} eyes and {my_hair} hair.")
+print(f"He's teeth are usually {my_teeth} depending on the food.")
+
+# this line is tricky, try to get it exactly right
+total = my_age + my_height + my_weight
+print(f"If i add {my_age}, {my_height} and {my_weight} I got {total}.")
