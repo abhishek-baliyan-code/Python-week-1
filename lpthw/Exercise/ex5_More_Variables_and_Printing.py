94e19f6ee0eb43396e651e756f4df02ba7983e21
Abhishek Baliyan <abhishek.baliyan.code@gmail.com>
2020-08-22 13:01:30 +0530

    Example 5 - Study Drills

--- lpthw/Exercise/ex5_More_Variables_and_Printing.py
+++ lpthw/Exercise/ex5_More_Variables_and_Printing.py
@@ -1,18 +1,19 @@
-my_name = 'Abhishek Baliyan'
-my_age = 26
-my_height = 5.7
-my_weight = 57
-my_eyes = 'Black'
-my_teeth = 'White'
-my_hair = 'Black'
+name = 'Abhishek Baliyan'
+age = 26
+height = 5.7
+weight = 57
+weight_in_pounds = weight * 2.20
+eyes = 'Black'
+teeth = 'White'
+hair = 'Black'
 
-print(f"Let's talk about {my_name}.")
-print(f"He's {my_height} foot tall.")
-print(f"He's {my_weight} kg heavy.")
+print(f"Let's talk about {name}.")
+print(f"He's {height} foot tall.")
+print(f"He's {weight} kg or {weight_in_pounds} pounds heavy.")
 print("Actually that's not too heavy.")
-print(f"He's got {my_eyes} eyes and {my_hair} hair.")
-print(f"He's teeth are usually {my_teeth} depending on the food.")
+print(f"He's got {eyes} eyes and {hair} hair.")
+print(f"He's teeth are usually {teeth} depending on the food.")
 
 # this line is tricky, try to get it exactly right
-total = my_age + my_height + my_weight
-print(f"If i add {my_age}, {my_height} and {my_weight} I got {total}.")
+total = age + height + weight
+print(f"If i add {age}, {height} and {weight} I got {total}.")
