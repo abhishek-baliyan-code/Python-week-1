9fce7655997d7401d230f43a847f7a132186129e
Abhishek Baliyan <abhishek.baliyan.code@gmail.com>
2020-08-22 19:25:54 +0530

    Example 7 - More Examples on Printing

---
+++ lpthw/Exercise/ex7_More_Printing.py
@@ -0,0 +1,19 @@
+print("Mary had a little lamb.")
+print("Its fleece was white as {}".format("snow"))
+print("And everywhere that Mary went.")
+print("." * 10)
+
+end1 = "A"
+end2 = "P"
+end3 = "P"
+end4 = "L"
+end5 = "E"
+end6 = "J"
+end7 = "U"
+end8 = "I"
+end9 = "C"
+end10 = "E"
+
+# watch that comma at the end. try removing it to see what happens
+print(end1 + end2 + end3 + end4 + end5, end=' ')
+print(end6 + end7 + end8 + end9 + end10)
